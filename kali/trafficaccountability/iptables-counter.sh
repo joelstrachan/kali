deda01a09a4fe0d314ea715fd9eef852921cf5e8
root <root@localhost.localdomain>
2019-12-27 01:35:25 -0500

    this is a new file hey

--- kali/trafficaccountability/iptables-counter.sh
+++ kali/trafficaccountability/iptables-counter.sh
@@ -3,7 +3,7 @@
 # rest all counters adn iptables rules
 iptables -Z && iptables -F
 # measure incoming traffic to 10.11.9.155
-iptables -I INPUT 1 -s 10.11.9.155 -j ACCEPT
+iptables -I INPUT 1 -s 10.11.8.12 -j ACCEPT
 # measure outgoing traffic to 10.11.9.155
-iptables -I OUTPUT 1 -d 10.11.9.155 -j ACCEPT
+iptables -I OUTPUT 1 -d 10.11.8.12 -j ACCEPT
 
