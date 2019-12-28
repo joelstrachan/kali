08890fa7de09bcac1508bf29464da28ed2af17d7
joelstrachan <Eriamjh30>
2019-12-28 16:46:24 +1100

    smtpenum with input file

--- kali/smtpenum/smtpenum.py
+++ kali/smtpenum/smtpenum.py
@@ -6,7 +6,7 @@
 import socket, sys, fileinput, re, time
 from optparse import OptionParser
 
-usage =  "./%prog -t <target> -p <port> -i <inputfile>\nExample: ./%prog -t 74.52.252.187 -p 25 -f names.txt"
+usage =  "./%prog -t <target> -p <port> -f <inputfile>\nExample: ./%prog -t 74.52.252.187 -p 25 -f names.txt"
 parser = OptionParser(usage=usage)
 parser.add_option("-t", type="string",
 action="store", dest="target",
@@ -30,6 +30,9 @@ if len(sys.argv) != 7:
     print ("|---------------------------------------------------------------|\n")
 parser.print_help()
 sys.exit()
+
+
+
 try:
     names = open(sys.argv[6], "r")
 except(IOError):
@@ -47,41 +50,39 @@ def connect():
         connect=s.connect((host,port))
     except socket.timeout:
         print("\n[-] Server timed out")
-sys.exit(1)
-
-
-#except socket.error:
-#    print("\n[-] There was an error with the server")
-#sys.exit(1)
-#    print("[+] Connected on" +timer())
-#    print("[+] Waiting for SMTP banner")
-#banner=s.recv(1024)
-#    print banner
+        sys.exit(1)
+    except socket.error:
+        print("\n[-] There was an error with the server")
+        sys.exit(1)
+        print("[+] Connected on" +timer())
+        print("[+] Waiting for SMTP banner")
+        banner=s.recv(1024)
+        print (banner)
 
 def timer():
     now = time.localtime(time.time())
     return time.asctime(now)
 
-connect()
+    connect()
 
-for line in names:
-    s.send('VRFY '+line)
-    result=s.recv(1024)
-    bad = re.match("502",result)
-    bad1 = re.search("send some mail",result)
-    found = re.search("252",result)
-    notfound = re.match("550",result)
-if bad or bad1:
-    print("[-] This server is not vulnerable!")
-    sys.exit(1)
-elif notfound:
-    print("[-] Not found "+line)
-elif found:
-    print("[+] Found! "+line)
-if counter == 20:
-    s.close()
-    print("[+] Resetting connection")
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    for line in names:
+        s.send('VRFY '+line)
+        result=s.recv(1024)
+        bad = re.match("502",result)
+        bad1 = re.search("send some mail",result)
+        found = re.search("252",result)
+        notfound = re.match("550",result)
+    if bad or bad1:
+        print("[-] This server is not vulnerable!")
+        sys.exit(1)
+    elif notfound:
+        print("[-] Not found "+line)
+    elif found:
+        print("[+] Found! "+line)
+    if counter == 20:
+        s.close()
+        print("[+] Resetting connection")
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 connect()
 counter = 0
 counter +=1
