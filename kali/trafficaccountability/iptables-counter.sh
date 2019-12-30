fbc11cbdffba078f8b54083c2e1f7ce9688bfb0b
joelstrachan <Eriamjh30>
2019-12-30 17:26:00 +1100

    Python buffer file

--- kali/trafficaccountability/iptables-counter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# rest all counters adn iptables rules
-iptables -Z && iptables -F
-# measure incoming traffic to 10.11.9.155
-iptables -I INPUT 1 -s 10.11.8.12 -j ACCEPT
-# measure outgoing traffic to 10.11.9.155
-iptables -I OUTPUT 1 -d 10.11.8.12 -j ACCEPT
-
